3da18985abd57ba54eebb770dbe84e6f7ae15dcd
Marko <manzikki@gmail.com>
2019-12-24 07:50:28 +0200

    count requests by date in apache logs

---
+++ count_by_date.sh
@@ -0,0 +1,5 @@
+awk '{ print $4 }' | sed 's/\[//' | sed 's/:.*//' \
+| sed 's/Jan/-01-/' | sed 's/Feb/-02-/' | sed 's/Mar/-03-/' | sed 's/Apr/-04-/' | sed 's/May/-05-/' \
+| sed 's/Jun/-06-/' | sed 's/Jul/-07-/' | sed 's/Aug/-08-/' | sed 's/Sep/-09-/' | sed 's/Oct/-10-/' \
+| sed 's/Nov/-11-/' | sed 's/Dec/-12-/' | sed 's/\///g' | sort -n -t"-" -k3 -k2 -k1 | uniq -c
+
